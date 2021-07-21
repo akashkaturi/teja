e1670724261b02ab2c44a9330d2384e134f8e3fb
akash <akashkaturi77@gmail.com>
2021-07-21 17:44:50 +0530

    readmore

--- javascript/nav.js
+++ javascript/nav.js
@@ -33,3 +33,48 @@ function scrollToTop(){
         behavior: 'smooth'
     })
 }
+
+
+var i = 0;
+function read(x){
+    
+    let moreField=".more"+String(x);
+    let imageField=".img"+String(x);
+    let dotsField=".dots"+String(x);
+    let text=".read"+String(x);
+    console.log(text);
+    console.log(moreField);
+    console.log(imageField);
+    console.log(dotsField);
+    if (x === 5) {
+         if (!i) {
+				document.querySelector(moreField).style.display = 'inline';
+				document.querySelector(dotsField).style.display = 'none';
+				document.querySelector(text).innerText = 'Read Less';
+				i = 1;
+			} else {
+				document.querySelector(moreField).style.display = 'none';
+				document.querySelector(dotsField).style.display = 'inline';
+				document.querySelector(text).innerText = 'Read More';
+
+				i = 0;
+			}
+	}
+    else{
+		if (!i) {
+			document.querySelector(moreField).style.display = 'inline';
+			document.querySelector(imageField).style.display = 'block';
+			document.querySelector(dotsField).style.display = 'none';
+			document.querySelector(text).innerText = 'Read Less';
+			i = 1;
+		} else {
+			document.querySelector(moreField).style.display = 'none';
+			document.querySelector(imageField).style.display = 'none';
+			document.querySelector(dotsField).style.display = 'inline';
+			document.querySelector(text).innerText = 'Read More';
+
+			i = 0;
+		}
+	}
+    
+}
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// // import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const element = <h1>Hello world 2</h1>;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+// function hello(){
+//   for(var i=0; i<=10;i++)
+//   {
+//     console.log(i**i);
+//   }
+//   console.log(i);
+// }
+
+const person={
+  name: 'Akash',
+  talk() {},
+  walk() {
+    console.log('this',this);
+  },
+}
+
+
+
+const target_member = 'name';
+person[target_member]='akash';
+person.walk();
+// const walk=person.walk.bind(person); /* bind function */
+// walk();
+// console.log(person.name);
+
+
+
+const square = (number)=> number*number; //arrow function.
+console.log(square(4));
+
+
+const jobs = [
+{ id:1, isActive: true},
+{ id:2, isActive: false},
+{ id:3, isActive: true},
+];
+
+// const activeJobs = jobs.filter(function(job){job.isActive});
+const activeJobs = jobs.filter(job=>job.isActive);
+// console.log(activeJobs);
+
+//Map function
+person.walk();
+const colors = ['red','blue','green','black'];
+const col=colors.map(color=>`<li> ${color} </li>`)
+console.log(col);
+
+//Object destructuring
+const address={
+  street:'Ramalingeshwara',
+  city:'',
+  country:'',
+};
+
+const {street, city, country}=address;
+console.log(street);
+
+//Concatenation using spread operator
+
+const first = [1,2,3];
+const second =[ 4,5,6];
+const combined = [...first,'a',...second];
+const clone_first = [...first];
+console.log(combined);
+console.log(clone_first);
